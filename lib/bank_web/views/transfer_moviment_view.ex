9ca127395cd31b1ec6c47651ac8d588c36403753
Jaqueline <jakiduque@gmail.com>
2019-12-08 17:50:24 -0300

    Changing json field on transfer.

--- lib/bank_web/views/transfer_moviment_view.ex
+++ lib/bank_web/views/transfer_moviment_view.ex
@@ -3,11 +3,17 @@ defmodule BankWeb.TransferMovimentView do
   alias BankWeb.TransferMovimentView
 
   def render("index.json", %{transfer_moviment: transfer_moviment}) do
-    %{data: render_many(transfer_moviment, TransferMovimentView, "transfer_moviment.json")}
+    %{
+      transfer_moviments:
+        render_many(transfer_moviment, TransferMovimentView, "transfer_moviment.json")
+    }
   end
 
   def render("show.json", %{transfer_moviment: transfer_moviment}) do
-    %{data: render_one(transfer_moviment, TransferMovimentView, "transfer_moviment.json")}
+    %{
+      transfer_moviment:
+        render_one(transfer_moviment, TransferMovimentView, "transfer_moviment.json")
+    }
   end
 
   def render("transfer_moviment.json", %{transfer_moviment: transfer_moviment}) do
