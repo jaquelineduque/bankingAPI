aed15953b601b65d48dccf88b87a8c846ed1c069
Jaqueline <jakiduque@gmail.com>
2019-12-08 18:32:00 -0300

    Changing balance json.

--- lib/bank_web/views/account_balance_view.ex
+++ lib/bank_web/views/account_balance_view.ex
@@ -3,15 +3,18 @@ defmodule BankWeb.AccountBalanceView do
   alias BankWeb.AccountBalanceView
 
   def render("index.json", %{account_balance: account_balance}) do
-    %{data: render_many(account_balance, AccountBalanceView, "account_balance.json")}
+    %{accounts_balance: render_many(account_balance, AccountBalanceView, "account_balance.json")}
   end
 
   def render("show.json", %{account_balance: account_balance}) do
-    %{data: render_one(account_balance, AccountBalanceView, "account_balance.json")}
+    %{account_balance: render_one(account_balance, AccountBalanceView, "account_balance.json")}
   end
 
   def render("account_balance.json", %{account_balance: account_balance}) do
-    %{id: account_balance.id, balance_amount: account_balance.balance_amount}
+    %{
+      account_register_id: account_balance.account_register_id,
+      balance_amount: account_balance.balance_amount
+    }
   end
 
   def render("error.json", %{error: error}) do
